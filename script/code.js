f566ee247212c74686efbf8fd76b9af4f468ffd0
shakeel <shakeeldvd23@gmail.com>
2024-05-17 15:46:20 +0200

    Coding Exercise

---
+++ script/code.js
@@ -0,0 +1,17 @@
+let person1 = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+let person2 = {
+    name: "Alice",
+    age: 25,
+    city: "Los Angeles"
+};
+
+console.log(person1);
+
+let peopleArray = [person1, person2];
+
+console.log(peopleArray);
